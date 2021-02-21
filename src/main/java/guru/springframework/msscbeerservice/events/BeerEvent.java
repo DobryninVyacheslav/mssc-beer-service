7540c1fa39af17fa5c84abd6b402f5e82b401c22
Vyacheslav Dobrynin <dobrinin_v_j@ntik.ru>
2021-02-21 11:33:30 +0300

    jackson JMS config

--- src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -1,19 +1,21 @@
 package guru.springframework.msscbeerservice.events;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @Builder
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class BeerEvent implements Serializable {
 
     private static final long serialVersionUID = -8063133939587132163L;
 
-    private final BeerDto beerDto;
+    private BeerDto beerDto;
 
 }
